36da9630844dfce39bc26cf0dc2bb8d3a8609a41
maxtm13 <maxtm1@yandex.ru>
2024-08-07 23:22:51 +0300

    header in process2

--- local/templates/colorit/header.php
+++ local/templates/colorit/header.php
@@ -35,25 +35,21 @@ $CurUri = $APPLICATION->GetCurUri();
                 <img src="" alt="" class="header__logo_pict">
             </div>
             <nav class="header__menu">
-                <?$APPLICATION->IncludeComponent(
-                    "bitrix:menu",
-                    ".default",
-                    array(
-                        "ALLOW_MULTI_SELECT" => "N",
-                        "CHILD_MENU_TYPE" => "left",
-                        "DELAY" => "N",
-                        "MAX_LEVEL" => "1",
-                        "MENU_CACHE_GET_VARS" => array(
-                        ),
-                        "MENU_CACHE_TIME" => "3600",
-                        "MENU_CACHE_TYPE" => "N",
-                        "MENU_CACHE_USE_GROUPS" => "Y",
-                        "ROOT_MENU_TYPE" => "top",
-                        "USE_EXT" => "N",
-                        "COMPONENT_TEMPLATE" => ".default"
-                    ),
-                    false
-                );?>
+                <?$APPLICATION->IncludeComponent("bitrix:menu", "main-menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "1",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "N",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
 
 
             </nav>
