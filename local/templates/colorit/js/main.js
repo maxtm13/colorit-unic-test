8118bd83636d28e92c3a6344bab4bdf1a07eadf9
maxtm13 <maxtm1@yandex.ru>
2024-08-14 07:33:42 +0300

    search form js

--- local/templates/colorit/js/main.js
+++ local/templates/colorit/js/main.js
@@ -2,12 +2,22 @@ $(document).ready(function () {
     console.log("Ready!")
     const search = document.querySelector('.search_btn')
     const searchBox = document.querySelector('.header__search_wrapper')
-
+    const searchForm = document.querySelector('#title-search-input')
     search.addEventListener('click', (p)=>{
         p.preventDefault();
-        searchBox.classList.toggle('active')
+        p.stopPropagation();
+        if (!searchBox.classList.contains('active'))
+            searchBox.classList.add('active');
     })
-    const body = document.getElementsByTagName('body');
-    body.addEventli
-
+    const body = document.querySelector('body')
+    document.addEventListener('keydown', (e)=>{
+        if (e.code=='Escape') {
+            searchBox.classList.remove('active')
+        }
+    })
+    document.addEventListener('click', (e)=>{
+        if (searchBox.classList.contains('active') && e.target!==searchForm) {
+            searchBox.classList.remove('active')
+        }
+    })
 });
