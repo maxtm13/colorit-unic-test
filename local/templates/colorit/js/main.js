94aacdbb3f5b47e283f77bf6ddd66c717ef9b991
maxtm13 <maxtm1@yandex.ru>
2024-08-19 18:09:35 +0300

    edit search - main.js

--- local/templates/colorit/js/main.js
+++ local/templates/colorit/js/main.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    console.log("Ready!")
     const search = document.querySelector('.search_btn')
     const searchBox = document.querySelector('.header__search_wrapper')
     const searchForm = document.querySelector('#title-search-input')
     search.addEventListener('click', (p)=>{
         p.preventDefault();
         p.stopPropagation();
-        if (!searchBox.classList.contains('active'))
-            searchBox.classList.add('active');
+            searchBox.classList.toggle('active');
     })
     const body = document.querySelector('body')
     document.addEventListener('keydown', (e)=>{
